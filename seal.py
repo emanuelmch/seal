428b47dbe47a29e373676d6d4ff379eb6c26500e
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2013-06-26 23:20:35 -0300

    ImageProcessing: Add the seal to the uploaded image

--- seal.py
+++ seal.py
@@ -1,10 +1,12 @@
 # Imports
 import os
+import PythonMagick
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
 
 # Configuration
 UPLOAD_FOLDER = './uploads'
+STATIC_FOLDER = './static'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 # Create the "app" object
@@ -14,6 +16,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Functions
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+
+def append_name(filename):
+	name, ext = os.path.splitext(filename)
+	return name + '_processed' + ext
+
+def process_image(filename):
+	newFilename = append_name(filename)
+	image = PythonMagick.Image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	imagesize = '%sx%s' % (image.columns() * 0.5, image.rows() * 0.5)
+	print 'imagesize = %s' % imagesize
+	seal = PythonMagick.Image(os.path.join(STATIC_FOLDER, 'seal.svg'))
+	seal.type = image.type
+	seal.transparent('#FFFFFF')
+	seal.resize(imagesize)
+	image.composite(seal, image.columns() / 4, image.rows() / 4, PythonMagick.CompositeOperator.SrcOverCompositeOp)
+	image.write(os.path.join(app.config['UPLOAD_FOLDER'], newFilename))
+	return newFilename
 
 # Routes
 @app.route('/', methods=['GET', 'POST'])
@@ -28,12 +47,15 @@ def home():
 			print os.path.join(app.config['UPLOAD_FOLDER'], filename)
 			print "********************************************"
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('uploaded_file', filename=filename))
+			newFilename = process_image(filename)
+
+			return redirect(url_for('uploaded_file', filename=newFilename))
+			#return redirect(url_for('uploaded_file', filename=filename))
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-		
+
 # Run! Run for your life!
 if __name__ == '__main__':
 	app.debug = True
--- static/dragdrop.js
+++ static/dragdrop.js
@@ -1,36 +1,36 @@
 function DragDrop_init() {
 	var doc = document.body;
-	
+
 	doc.ondragenter = function () {
 		console.log('ondragenter');
 		this.className = 'fileHover';
 		return false;
 	}
-	
+
 	doc.ondragover = function () {
 		return false;
 	}
-	
+
 	doc.ondragleave = function() {
 		console.log('ondragleave');
 		this.className = '';
 		return false;
 	}
-	
+
 	doc.ondrop = function(event) {
 		console.log('ondrop');
 		event.stopPropagation && event.stopPropagation();
 		event.preventDefault && event.preventDefault();
 		this.className = '';
-		
+
 		var files = event.dataTransfer.files;
 		document.forms.fileUploading.file.files = files;
 		document.forms.fileUploading.submit();
-		
+
 		return false;
 	}
-	
-	
+
+
 
 	console.log('Init OK');
 }
